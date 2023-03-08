ccecbded2df637c969c3e1a133a1ba06fa2d60b2
Dynesshely <catrol@qq.com>
2023-03-09 06:47:15 +0800

    💾 Feat(Platform): Updated

--- Platform.cs
+++ Platform.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace KeysHandler.Models;
+﻿namespace KeysHandler.Models;
 
 public class Platform
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public List<String> URLs { get; set; }
+    public List<string>? UrlList { get; set; }
 
-    public List<VerifyRecord> VerifyRecords { get; set; }
+    public List<VerifyRecord>? VerifyRecords { get; set; }
 }
-
