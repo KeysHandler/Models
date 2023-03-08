03d1eec873b8999b54c56b45867f924ba790cfb3
Dynesshely <catrol@qq.com>
2023-03-09 06:36:50 +0800

    💾 Feat: Platform model.

---
+++ Platform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeysHandler.Models;
+
+public class Platform
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public List<String> URLs { get; set; }
+
+    public List<VerifyRecord> VerifyRecords { get; set; }
+}
+
