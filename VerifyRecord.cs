1ed80e92eb2fb14a53fa597a2f5750f3448053cf
Dynesshely <catrol@qq.com>
2023-03-09 06:37:04 +0800

    💾 Feat: VerifyRecord model.

---
+++ VerifyRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeysHandler.Models
+{
+    public class VerifyRecord
+    {
+        public List<string> Names { get; set; }
+
+        public List<string> E_Mails { get; set; }
+
+        public List<string> PhoneNumbers { get; set; }
+    }
+}
